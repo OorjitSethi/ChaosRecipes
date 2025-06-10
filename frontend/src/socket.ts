859f12616a076f97d63e58fef4201fc88e93a47d
“OorjitSethi” <sethioorjit2008@gmail.com>
2025-06-10 21:06:25 +0530

    prepared for deployment

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,7 +1,7 @@
 // frontend/src/socket.ts
 import { io, Socket } from "socket.io-client";
 
-const URL = "http://localhost:3001"; // Your backend server URL
+const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3001"; // Your backend server URL
 export const socket: Socket = io(URL, {
     autoConnect: false // We will connect manually
 });
